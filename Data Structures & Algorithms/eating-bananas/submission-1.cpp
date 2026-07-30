3b7ac3c5499809a69e1869ed9f9bdfda5e4dd795
Ujjwal <uk934010@gmail.com>
2026-07-30 10:37:03 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int getCount(vector<int>& piles, int mid){
+        int sum =0;
+        for(int i=0;i<piles.size();i++){
+            sum += (piles[i]+ mid-1) / mid;
+        }
+        return sum;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = INT_MIN;
+        for(int i=0;i<piles.size();i++){
+            high = max(high,piles[i]);
+        }
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            int totalHours = getCount(piles,mid);
+            if(totalHours<= h){
+                high = mid-1;
+            }else low = mid+1;
+        }
+        return low;
+    }
+};
